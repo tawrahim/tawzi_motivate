f1b4c3a6c6f9581758f0eca7b3a630298fa87ce0
Tawheed Abdul-Raheem <tawrahim@gmail.com>
2012-12-05 19:48:23 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TawziMotivate::Application.config.secret_token = '2e17783adfa7d324cae12413a99d82600347f0b2f630fe80a82f97e32ea7bc7b06fd3b9691fd291786489d0baabb2da33b359273295541d603d6e5b7d9dbd2af'
